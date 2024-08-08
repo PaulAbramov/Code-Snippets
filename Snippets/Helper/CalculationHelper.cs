d7c637e6d4d2b67c06f11d1674a05d357b513129
Paul Abramov <pa@jamoin.de>
2024-08-08 12:31:47 +0200

    Add Calculationhelper

---
+++ Snippets/Helper/CalculationHelper.cs
@@ -0,0 +1,44 @@
+﻿namespace Snippets.Helper
+{
+    public class CalculationHelper
+    {
+        #region Divide
+
+        public static int Divide(int dividend, int divisor, bool throwError = false)
+        {
+            return Divide<int>(dividend, divisor, throwError);
+        }
+
+        public static long Divide(long dividend, long divisor, bool throwError = false)
+        {
+            return Divide<long>(dividend, divisor, throwError);
+        }
+
+        public static decimal Divide(decimal dividend, decimal divisor, bool throwError = false)
+        {
+            return Divide<decimal>(dividend, divisor, throwError);
+        }
+
+        public static double Divide(double dividend, double divisor, bool throwError = false)
+        {
+            return Divide<double>(dividend, divisor, throwError);
+        }
+
+        public static T Divide<T>(T dividend, T divisor, bool throwError = false) where T : struct
+        {
+            if (throwError && (dynamic)divisor == 0)
+            {
+                throw new DivideByZeroException();
+            }
+
+            if ((dynamic)divisor == 0)
+            {
+                return default(T);
+            }
+
+            return (T)Convert.ChangeType((dynamic)dividend / (dynamic)divisor, typeof(T));
+        }
+
+        #endregion Divide
+    }
+}
--- Snippets/Program.cs
+++ Snippets/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var dividend = 10;
+            var divisor = 10;
+
+            var test = Helper.CalculationHelper.Divide(dividend, divisor);
+
+            divisor = 0;
+            var test3 = Helper.CalculationHelper.Divide(dividend, divisor);
+            try
+            {
+                var test2 = Helper.CalculationHelper.Divide(dividend, divisor, true);
+            }
+            catch (DivideByZeroException ex)
+            {
+                System.Console.WriteLine(ex.Message);
+            }
         }
     }
 }
